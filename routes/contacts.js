31d09dc871b3684dcbddc4aeee903af316659c9a
anilSonix <anil.sonix@gmail.com>
2023-07-25 23:25:15 +0530

    adding basic sqlite3 db

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,54 +1,94 @@
 var express = require("express");
+const { db } = require("../db");
 var router = express.Router();
 
-const contacts = [
-  {
-    id: 1,
-    name: "anil",
-    phone: "8602408135",
-  },
-];
-
 router.get("/", function (req, res, next) {
-  res.json(contacts);
+  db.all("SELECT rowid , name , phone from contacts;", [], (err, rows) => {
+    if (err) {
+      console.log(err.message);
+      return res.status(500).json({ errors: [err.message] });
+    }
+    console.log(rows);
+    res.json(rows.map((c) => ({ id: c.rowid, name: c.name, phone: c.phone })));
+  });
 });
 
 router.get("/:id", function (req, res, next) {
-  const contact = contacts.find((c) => c.id == req.params.id);
-  if (!contact) {
-    return res.sendStatus(400);
-  }
-  res.json(contact);
+  db.get(
+    "SELECT rowid , name , phone from contacts where rowid = ?  limit 1;",
+    [req.params.id],
+    (err, row) => {
+      if (err) {
+        console.log(err.message);
+        return res.status(500).json({ errors: [err.message] });
+      }
+      console.log(row);
+      if (!row) {
+        return res.status(400).json({ errors: ["Bad contact id"] });
+      }
+      res.json({ id: row.rowid, name: row.name, phone: row.phone });
+    }
+  );
 });
 
 router.post("/", function (req, res, next) {
-  const contact = {
-    id: Date.now(),
-    name: req.body.name,
-    phone: req.body.phone,
-  };
   contacts.push(contact);
-  res.json(contact);
+  db.run(
+    "INSERT INTO contacts values (?,?)",
+    [req.body.name, req.body.phone],
+    function (err) {
+      if (err) {
+        console.log(err.message);
+        return res.status(500).json({ errors: [err.message] });
+      }
+      console.log(`A row has been inserted with rowid`, this);
+      res.status(201).json({ id: this.lastID });
+    }
+  );
 });
 
 router.put("/:id", function (req, res, next) {
-  const index = contacts.findIndex((c) => c.id == req.params.id);
-  if (index == -1) {
-    return res.sendStatus(400);
-  }
-  contacts[index].name = req.body.name;
-  contacts[index].phone = req.body.phone;
-
-  res.json(contacts[index]);
+  db.run(
+    "UPDATE contacts SET name = ? , phone = ?  WHERE ROWID = ?",
+    [req.body.name, req.body.phone, req.params.id],
+    function (err) {
+      if (err) {
+        console.log(err.message);
+        return res.status(500).json({ errors: [err.message] });
+      }
+      console.log(this);
+      if (this.changes != 1) {
+        return res.status(400).json({ errors: ["Bad contact id"] });
+      }
+      res.json({
+        id: +req.params.id,
+        name: req.body.name,
+        phone: req.body.phone,
+      });
+    }
+  );
 });
 
 router.delete("/:id", function (req, res, next) {
-  const index = contacts.findIndex((c) => c.id == req.params.id);
-  if (index == -1) {
-    return res.sendStatus(400);
-  }
-  contacts.splice(index, 1);
-  res.json(contacts);
+  db.run(
+    "DELETE FROM contacts  WHERE ROWID = ?",
+    [req.params.id],
+    function (err) {
+      if (err) {
+        console.log(err.message);
+        return res.status(500).json({ errors: [err.message] });
+      }
+      console.log(this);
+      if (this.changes != 1) {
+        return res.status(400).json({ errors: ["Bad contact id"] });
+      }
+      res.json({
+        id: +req.params.id,
+        name: req.body.name,
+        phone: req.body.phone,
+      });
+    }
+  );
 });
 
 module.exports = router;
